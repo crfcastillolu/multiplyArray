3d2ac2df7156e9bc13385279a1acc038dbcbd7d3
paramo <paramo@paramo.local>
2016-05-13 13:31:17 -0500

    Multiplicación de valores en un arreglo de tres dimensiones

--- js/javascript.js
+++ js/javascript.js
@@ -46,7 +46,7 @@
 // 	}
 
 // var resultado = 0;
-// var numeros = [[1],[2],[3]];
+// var numeros = [[2],[2],[3]];
 
 // 	for( var i = 0; i <= numeros.length-1 ; i++){
 // 		resultado += numeros[i][0];
@@ -58,7 +58,11 @@ var resultado = 0;
 var numeros = [[1,2],[3,4],[5,6,7]];
 
 	for( var i = 0; i <= numeros.length-1 ; i++){
-		resultado += numeros[i][0][0];
+		resultado += numeros[i];
+			for(var j = 1 ; i < numeros.length[j] ; j++){
+				resultado += numeros[j];
+				console.log(resultado += numeros[j]);
+			}
 	}
 
 console.log(resultado);
